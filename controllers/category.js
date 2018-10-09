691ea7897f63ad2d8c301d91c2731536e5e33e58
denis <denis1999yaschenko@gmail.com>
2018-10-09 14:14:14 +0300

    endposition's functions, end category's function: getById, getAll remove

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,55 @@
-module.exports.getAll = function (req, res) {
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorhandler');
+const Position = require('../models/Position');
 
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({
+            user: req.user.id
+        });
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.getById = function (req, res) {
-    
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    } 
 }
 
-module.exports.remove = function (req, res) {
-    
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({
+            _id: req.params.id
+        });
+        await Position.remove({
+            category: req.params.id
+        });
+        res.status(200).json({
+            message: 'Category was deleted'
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }  
 }
 
 module.exports.create = function (req, res) {
-    
+    try {
+        
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.update = function (req, res) {
-    
+    try {
+        
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
