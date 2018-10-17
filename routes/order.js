41354601cbcb6045e6b4d9955382ae8bd5ef7b1f
Denis Yaschenko <deniskrut3233@gmail.com>
2018-10-17 17:05:12 +0300

    order => end functions getAll, create

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controller = require('../controllers/order');
 const router = express.Router();
+const passport = require('passport');
 
-router.post('/', controller.getAll);
-router.get('/', controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.getAll);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 module.exports = router
